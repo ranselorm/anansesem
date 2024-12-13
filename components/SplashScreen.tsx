2fdece25611547153273a8d5962a3b8955681b85
ranselorm <ranselorm@outlook.com>
2024-12-13 08:58:32 +0000

    First commit

---
+++ components/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, StyleSheet, Image} from 'react-native'
+
+const SplashScreen = () => {
+  return (
+   <View>
+    <Image />
+   </View>
+  )
+}
+
+export default SplashScreen
